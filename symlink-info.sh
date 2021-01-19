20c0e7c6be5e2dd98d8aa081d375b1217e137e10
William Andrea <william.j.andrea@gmail.com>
2021-01-18 22:02:41 -0500

    Add help and option parsing to "symlink-info"

--- symlink-info.sh
+++ symlink-info.sh
@@ -1,14 +1,64 @@
 #!/bin/bash
 # Resolve a symlink, recursively and canonically.
+#
+# See functions _symlink_info_usage and _symlink_info_help for more details.
+
+function _symlink_info_help {
+    _symlink_info_usage
+    echo
+    cat <<'EOF'
+Resolve a symlink, recursively and canonically.
+
+Arguments:
+    FILE    Filename of symlink to resolve.
+
+Options:
+    -h      Print this help message and exit.
+
+Info provided per symlink:
+    - target, recursively
+    - (if relative: canonical path)
+
+Exit Status:
+    3 - Invalid options
+    1 - At least one FILE is not found, or any other error
+    0 - otherwise
+EOF
+}
+
+function _symlink_info_usage {
+    printf 'Usage: %s [-h] [file ...]\n' "$basename"
+}
 
 function symlink_info { (
     source indenter.sh || exit 1  # Get function "indenter"
 
-    # Basename of caller, for error messages
+    # Basename of caller, for error messages and help
     basename=$(basename -- "$0")
     # Name of the main function, for error messages.
     funcname="${FUNCNAME[0]}"
+
+    # Defaults
     exit=0
+
+    OPTIND=1
+    while getopts :h OPT; do
+        case $OPT in
+        h)
+            _symlink_info_help
+            exit 0
+            ;;
+        *)
+            printf >&2 '%s: %s: Invalid option: -%s\n' \
+                "$basename" \
+                "$funcname" \
+                "$OPTARG"
+            _symlink_info_usage >&2
+            exit 3
+            ;;
+        esac
+    done
+    shift "$((OPTIND-1))"
 
     for path; do
         problem=
